f095f6532119b490214e6c4f9aed560c5e64f64d
Andres <afsanchez93@gmail.com>
2020-11-13 18:21:58 -0500

    Agregando modulo de informes y graficas

--- mediciones/models.py
+++ mediciones/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 # Create your models here.
 class Medida(models.Model):
+    """
+    Clase que representa el modelo de una medida en la base de datos
+    """
     fecha = models.DateTimeField()
     presion = models.FloatField()
     humedad = models.FloatField()
