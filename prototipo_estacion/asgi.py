80c883597a40f62765e5c4308039448373661d87
Andres <afsanchez93@gmail.com>
2020-11-13 13:04:56 -0500

    Commit inicial

---
+++ prototipo_estacion/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for prototipo_estacion project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prototipo_estacion.settings')
+
+application = get_asgi_application()
