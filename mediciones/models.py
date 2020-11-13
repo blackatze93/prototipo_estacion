bebf64649eccf76162d87515ba8c391166c86e55
Andres <afsanchez93@gmail.com>
2020-11-13 17:45:19 -0500

    Configurando el modelo de mediciones

--- mediciones/models.py
+++ mediciones/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Medida(models.Model):
+    fecha = models.DateTimeField()
+    presion = models.FloatField()
+    humedad = models.FloatField()
+    temperatura = models.FloatField()
+    co2_ppm = models.FloatField()
+    latitud = models.FloatField()
+    longitud = models.FloatField()
