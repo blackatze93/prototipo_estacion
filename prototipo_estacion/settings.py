0f86c2bbf66aada23f2e1aaaa11d69fcf9ef4831
Andres <afsanchez93@gmail.com>
2021-04-07 00:01:07 -0500

    Agregando STATIC_ROOT

--- prototipo_estacion/settings.py
+++ prototipo_estacion/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
 
 # Using Google Drive as File Storage System
