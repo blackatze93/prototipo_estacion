b507a7d8a85bc6c153f05aca7a32ed3b9e1d544b
Andres <afsanchez93@gmail.com>
2021-06-27 21:19:31 -0500

    Agregando librerias locales

--- prototipo_estacion/settings.py
+++ prototipo_estacion/settings.py
@@ -119,10 +119,14 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
